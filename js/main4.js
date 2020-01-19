98ce847d8d9ef74d286525e89ad57923dd358fbe
ichogo555 <58685690+ichogo555@users.noreply.github.com>
2020-01-19 17:35:40 +0900

    Add files via upload

--- js/main4.js
+++ js/main4.js
@@ -1,6 +1,11 @@
-
-
-
-
-
-
+_=0;
+      addEventListener('load', function(ev){
+      _ ; [].slice.apply(document.querySelectorAll('button.counting')).forEach(function(e){
+      _ , _ ; e.addEventListener('click', function(evt){
+      _ , _ , _ ; var a = parseInt(this.getAttribute('data-count'));
+      _ , _ , _ ; a = isNaN(a)? 1: a+1;
+      _ , _ , _ ; ccc.textContent = a+1;
+      _ , _ , _ ; this.setAttribute('data-count', a);
+      _ , _ ; }, false);
+      _ ; });
+      }, false);
